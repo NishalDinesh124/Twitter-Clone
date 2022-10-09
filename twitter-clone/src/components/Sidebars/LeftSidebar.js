59ae6b63ae7639a43c8b139ee9f83e200db69ad8
Nishal Dinesh <nishaldinesh3516j@gmail.com>
2022-10-09 16:51:57 +0530

    Left sidebar completed

--- twitter-clone/src/components/Sidebars/LeftSidebar.js
+++ twitter-clone/src/components/Sidebars/LeftSidebar.js
@@ -3,7 +3,78 @@ import './LeftSidebar.css'
 function LeftSidebar() {
   return (
     <div className='left-section'>
-      
+      <div className="twitter-icon">
+        <i class="fa-brands fa-twitter"></i>
+      </div>
+      <div className="panel">
+        <div className="home">
+          <i class="fa-solid fa-house-chimney-window"></i>
+          <div className="inner-home">
+          <a href="#"> Home </a>
+          </div>
+
+        </div>
+        <div className="home">
+          <div>
+          <i class="fa-solid fa-hashtag"></i>
+   
+          </div>
+                   <div className="inner-home">
+          <a href="#"> Explore </a>
+          </div>
+          
+        </div>
+        <div className="home">
+          <i class="fa-regular fa-bell"></i>
+          <div className="inner-home">
+          <a href="#"> Notifications </a>
+          </div>
+        </div>
+        <div className="home">
+          <i class="fa-regular fa-envelope"></i>
+          <div className="inner-home">
+          <a href="#"> Messages </a>
+          </div>
+        </div>
+        <div className="home">
+          <i class="fa-regular fa-bookmark"></i>
+          <div className="inner-home">
+          <a href="#"> Bookmarks </a>
+          </div>
+        </div>
+        <div className="home">
+          <i class="fa-solid fa-list-ul"></i>
+          <div className="inner-home">
+          <a href="#"> Lists </a>
+          </div>
+        </div>
+        <div className="home">
+          <i class="fa-regular fa-user"></i>
+          <div className="inner-home">
+          <a href="#"> Profile </a>
+          </div>
+        </div>
+        <div className="home">
+          <i class="fa-regular fa-comment-dots"></i>
+          <div className="inner-home">
+          <a href="#"> More </a>
+          </div>
+        </div>
+
+
+
+      </div>
+      <div className="tweet">
+       <button className='tweet-btn'>
+        Tweet
+       </button>
+      </div>
+
+
+      <div className="account">
+        
+      </div>
+
     </div>
   )
 }
