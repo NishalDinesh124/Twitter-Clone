14a9fb8a975a773391a5f03783fa07ab28cd1be4
Nishal Dinesh <nishaldinesh3516j@gmail.com>
2022-10-13 20:56:21 +0530

    Working on main section, Tweet section skeleton completed

--- twitter-clone/src/App.js
+++ twitter-clone/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="container">
       
-      
+      <Navbar/>
        <LeftSidebar/>
        <HomePage/>
       <RightSidebar/> 
--- twitter-clone/src/components/Homepage/Homepage.js
+++ twitter-clone/src/components/Homepage/Homepage.js
@@ -1,20 +1,34 @@
 import React from 'react'
 import './Homepage.css'
 function Homepage() {
-  return (
-    
-      <div className="main-section">
-       <div className="navbar">
-            <div className="nav-icon">
-            <h5>Home</h5>
+    return (
+
+        <div className="main-section">
+            <div className="navbar">
+                <div className="nav-icon">
+                    <h5>Home</h5>
+                </div>
+                <div>
+                    <i class="fa-solid fa-wand-magic-sparkles"></i>
+                </div>
+
             </div>
-            <div>
-                <i class="fa-regular fa-star ">
-                    </i></div>
-            
+            <div className="tweet-section">
+                <div className="left-panel">
+
+                </div>
+                <div className="right-panel">
+                    <div className="top-panel">
+
+                    </div>
+                    <div className="bottom-panel">
+
+                    </div>
+                </div>
+
+            </div>
         </div>
-      </div>
-  )
+    )
 }
 
 export default Homepage
